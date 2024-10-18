eb8bea865d296da444d19bb5a59f43beb5bdffeb
GitShreyaRay <marsvsjupiter@gmail.com>
2024-10-18 10:09:17 +0530

    My first commit

---
+++ src/main/java/com/nice/app/NiceCrsRestApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceCrsRestApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceCrsRestApiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nice/app/NiceCrsRestApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceCrsRestApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
